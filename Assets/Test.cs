5c8b5ecfa6e480a499d10b9c524e84b2a85035a7
Hitz <hachegamer25@gmail.com>
2024-02-04 23:34:42 +0000

    Back To Start

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Help : MonoBehaviour
+public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
