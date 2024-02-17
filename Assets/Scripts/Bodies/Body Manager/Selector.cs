9d2a53437226afe573610eaa1596901146828ad7
FaCe_Hitz <hachegamer25@gmail.com>
2024-02-17 12:51:13 +0000

    Slector Done

--- Assets/Scripts/Bodies/Body Manager/Selector.cs
+++ Assets/Scripts/Bodies/Body Manager/Selector.cs
@@ -7,6 +7,8 @@ public class Selector : MonoBehaviour
 {
     GameObject selector, info;
 
+    public float scaleFactor = 22.1f;
+
     RectTransform img;
 
     Image selectorImg;
@@ -32,11 +34,21 @@ public class Selector : MonoBehaviour
     }
 
     void activateSelector(){
-        if(info.activeInHierarchy && img != null && GameManager.selBodyOut != null){
 
+        if(info.activeInHierarchy && img != null && GameManager.selBodyOut != null){
+            //Sets position
             Vector2 bodyPos = Camera.main.WorldToScreenPoint(GameManager.selBodyOut.GetComponent<Transform>().position);
 
             img.position = bodyPos;
+
+            //Sets Size based on radius
+            float radius = GameManager.selBodyOut.GetComponent<Gravity>().b_radius;
+
+            img.sizeDelta = new Vector2(radius * scaleFactor, radius * scaleFactor);
+
+            if(GameManager.selBodyOut.GetComponent<Gravity>().isStar){
+                img.sizeDelta = new Vector2(radius * scaleFactor / 2f, radius * scaleFactor / 2f);
+            }
 
             selectorImg.gameObject.SetActive(true);
         }
